a1edc42bb0bbbd28e49144f970d7c2b1c3ff2dbc
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-12-26 00:47:54 -0500

    Create Application component to contain every major sub-piece of Timeline app

---
+++ src/Application.jsx
@@ -0,0 +1,12 @@
+import './Application.css'
+import TimelineGrid from "./TimelineGrid.jsx";
+
+function Application() {
+    return (
+        <>
+            <TimelineGrid size={25}/>
+        </>
+    );
+}
+
+export default Application
