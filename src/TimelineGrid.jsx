4c72984ff1ed3fdf13eb2303c2a36a7a73da4a8b
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-12-27 04:09:33 -0600

    Fix select to now actually select/deselect + update empty-plot's into timeline-entry's on click

--- src/TimelineGrid.jsx
+++ src/TimelineGrid.jsx
@@ -3,29 +3,39 @@ import {useState} from "react";
 function TimelineEntry(props){
 
     // eslint-disable-next-line react/prop-types
-    const entryClass = props.entryState ? "select" : "unselect";
+    let classStr = props.entrySelectState ? "select" : "deselect";
+    classStr += " ";
+    // eslint-disable-next-line react/prop-types
+    classStr += props.entryAddState ? "timeline-entry" : "";
+    const entryClass = classStr;
+
     // eslint-disable-next-line react/prop-types
     const onEntryClick = props.onEntryClick;
 
     return(
         // eslint-disable-next-line react/prop-types
-        <div onClick={onEntryClick} className={"timeline-entry" + " " + entryClass}></div>
+        <div onClick={onEntryClick} className={"timeline-plot" + " " + entryClass}></div>
     );
 }
 
 function GridItem(){
     const [entrySelect, setEntrySelect] = useState(false);
+    const [entryAdd, setEntryAdd] = useState(false);
 
-    const onEntryClick = (entrySelected) => () => {
+    const onEntryClick = (entrySelected, entryAdded) => () => {
         const nextEntrySelect = !entrySelected;
+        const nextEntryAdd = entryAdded || nextEntrySelect;
         setEntrySelect(nextEntrySelect);
+        setEntryAdd(nextEntryAdd);
     };
 
     return (
         <div className="grid-item">
             <div className="horizontal-line"></div>
             <div className="vertical-line"></div>
-            <TimelineEntry onEntryClick={onEntryClick(entrySelect)} entryState={entrySelect} />
+            <TimelineEntry onEntryClick={onEntryClick(entrySelect, entryAdd)}
+                           entrySelectState={entrySelect}
+                           entryAddState={entryAdd}/>
         </div>
     );
 }
