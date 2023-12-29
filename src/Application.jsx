b23b5e958dad89a95ede54ee4ed83a2289a3d8a5
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-12-29 14:39:03 -0600

    Add outline for EntryCard component with basic styling

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,10 +1,13 @@
 import './Application.css'
+import './EntryCard.css'
 import TimelineGrid from "./TimelineGrid.jsx";
+import EntryCard from "./EntryCard.jsx";
 
 function Application() {
     return (
         <>
             <TimelineGrid size={25}/>
+            <EntryCard/>
         </>
     );
 }
---
+++ src/EntryCard.jsx
@@ -0,0 +1,19 @@
+
+// function Image
+
+function EntryCard(){
+
+    return (
+        <>
+            <div className="card-container">
+                <div>Image</div>
+                <div>Title</div>
+                <div>Date</div>
+                <div>Description</div>
+            </div>
+        </>
+    );
+}
+
+// Exports
+export default EntryCard
