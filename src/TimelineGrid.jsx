7bd2ae56319844bdd762050d6e840351a896990f
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2023-12-28 20:32:46 -0600

    Add PropTypes for prop validation

--- src/TimelineGrid.jsx
+++ src/TimelineGrid.jsx
@@ -1,33 +1,28 @@
 import {useEffect, useState} from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
 function TimelineEntry(props){
 
-    // eslint-disable-next-line react/prop-types
     let classStr = props.entrySelectState ? "select" : "deselect";
     classStr += " ";
-    // eslint-disable-next-line react/prop-types
     classStr += props.entryAddState ? "timeline-entry" : "";
-    const entryClass = classStr;
 
-    // eslint-disable-next-line react/prop-types
+    const entryClass = classStr;
     const onEntryClick = props.onEntryClick;
 
     return(
-        // eslint-disable-next-line react/prop-types
         <div onClick={onEntryClick} className={"timeline-plot" + " " + entryClass}></div>
     );
 }
 
+
 function GridItem(props){
-    // eslint-disable-next-line react/prop-types
+
     const [entrySelect, setEntrySelect] = useState(props.selectGridItemKey === props.gridKey);
     const [entryAdd, setEntryAdd] = useState(false);
 
     useEffect(() => {
-        // eslint-disable-next-line react/prop-types
         setEntrySelect(props.selectGridItemKey === props.gridKey)
-        // eslint-disable-next-line react/prop-types
     }, [props.gridKey, props.selectGridItemKey]);
 
     const onEntryClick = (entrySelected, entryAdded) => () => {
@@ -35,7 +30,6 @@ function GridItem(props){
         const nextEntryAdd = entryAdded || nextEntrySelect;
         setEntrySelect(nextEntrySelect);
         setEntryAdd(nextEntryAdd);
-        // eslint-disable-next-line react/prop-types
         props.setSelectGridItemKey(props.gridKey);
     };
 
@@ -50,17 +44,19 @@ function GridItem(props){
     );
 }
 
+
 function TimelineGrid(props){
+
     const [selectGridItemKey, setSelectGridItemKey] = useState(null);
 
     // Create an array of `GridItem` components
     const gridItems = [];
-    // eslint-disable-next-line react/prop-types
     for (let i = 1; i <= props.size; i++) {
+        // Key added for React list rendering
         gridItems.push(<GridItem key={i}
                                  selectGridItemKey={selectGridItemKey}
                                  setSelectGridItemKey={setSelectGridItemKey}
-                                 gridKey={i} />);  // Key added for React list rendering
+                                 gridKey={i} />);
     }
 
     return (
@@ -72,12 +68,27 @@ function TimelineGrid(props){
     );
 }
 
-// TimelineGrid.propTypes = {
-//     side: PropTypes.number.isRequired,
-// };
-//
-// TimelineGrid.defaultProps = {
-//     side: 25,
-// };
 
+// PropTypes setup
+TimelineGrid.propTypes = {
+    size: PropTypes.number.isRequired,
+};
+
+TimelineGrid.defaultProps = {
+    size: 25,
+};
+
+GridItem.propTypes = {
+    selectGridItemKey: PropTypes.number.isRequired,
+    setSelectGridItemKey: PropTypes.func.isRequired,
+    gridKey: PropTypes.number.isRequired,
+};
+
+TimelineEntry.propTypes = {
+    onEntryClick: PropTypes.func,
+    entrySelectState: PropTypes.bool,
+    entryAddState: PropTypes.bool,
+};
+
+// Exports
 export default TimelineGrid
