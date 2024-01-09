c56d4f8b07b4b69b7a1813f068015bacfb019115
Swarup Ukil <77638087+SwarupUkil@users.noreply.github.com>
2024-01-08 18:39:04 -0600

    Improve panning of timeline grid + setup draggable entry's

--- src/Application/Application.jsx
+++ src/Application/Application.jsx
@@ -2,38 +2,57 @@ import TimelineGrid from "../TimelineGrid/TimelineGrid.jsx";
 import EntryCard from "../EntryCard/EntryCard.jsx";
 import DeleteEntryModal from "../DeleteEntryModal/DeleteEntryModal.jsx";
 import SideBar from "../EntryCard/Sidebar.jsx";
-import {useState, createContext, useEffect} from "react";
+import {useState, createContext} from "react";
 import Header from "../Header/Header.jsx";
 import useDeleteEntryLogic from "../DeleteEntryModal/useDeleteEntryLogic.js"
 import useConnectEntriesLogic from "../ConnectEntry/useConnectEntriesLogic.js";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 export const EntryCardContext = createContext({
     visibilityValue: "hidden",
     setVisibilityValue: () => {},
-    keyValue: 0,
-    setKeyValue: () => {},
     isSelected: false,
     setIsSelected: () => {},
+
     deleteState: false,
     setDeleteState: () => {},
+
     currentView: "edit-mode",
+
     connectState: null,
     setConnectState: null,
+
+    entryIdCounter: 1,
+    setEntryIdCounter: () => {},
+    entryNumbers: [],
+    setEntryNumbers: () => {},
+    selectedEntryId: null,
+    setSelectedEntryId: () => {},
+    setDisableDrag: () => {},
 });
 
 function Application() {
-    const size = 45; // Timeline grid size
+    const size = 81; // Timeline grid size
     const [visibilityValue, setVisibilityValue] = useState("hidden"); // ID for entry card component visibility
-    const [keyValue, setKeyValue] = useState(0); // Entry card key value to be accessed
     const [selectGridItemKey, setSelectGridItemKey] = useState(null); // Currently selected entry's key
     const [isSelected, setIsSelected] = useState(false); // True if any entry is selected
     const [currentView, setCurrentView] = useState("edit-mode"); // Contains the current view
 
+    // delete-entry use case states
     const {deleteState, setDeleteState,
         deleteModalState, confirmDelete} = useDeleteEntryLogic(currentView, isSelected);
+
+    // connect-entry use case states
     const {connectState, setConnectState, connectSpecificEntries,
         connectEntries} = useConnectEntriesLogic(size, currentView);
+
+    // Timeline Entry states
+    const [entryIdCounter, setEntryIdCounter] = useState(1); // ID incrementer for each new entry
+    const [entryNumbers, setEntryNumbers] = useState([]); // current list of entries active
+    const [selectedEntryId, setSelectedEntryId] = useState(1); // ID of entry that is selected
+    const [disableDrag, setDisableDrag] = useState(false); // Disables timeline grid panning
 
 
     return (
@@ -46,26 +65,33 @@ function Application() {
 
             {/* Main Content Region */}
             <div id={"timeline-wrapper"}>
-                <EntryCardContext.Provider value={{visibilityValue, setVisibilityValue, keyValue, setKeyValue,
-                    isSelected, setIsSelected, deleteState, setDeleteState, currentView, connectState, setConnectState}}>
+
+                <EntryCardContext.Provider value={{visibilityValue, setVisibilityValue,
+                    isSelected, setIsSelected, deleteState, setDeleteState, currentView, connectState, setConnectState,
+                    entryIdCounter, setEntryIdCounter, entryNumbers, setEntryNumbers, selectedEntryId, setSelectedEntryId, setDisableDrag}}>
                     <div className="fill-container"></div>
 
-                    <div className={"timeline-content"}>
-                            <TransformWrapper options={{ limitToBounds: false, centerContent: false, centerOnInit: false, initialScale: 1,
-                                                  minScale: 0.5,
-                                                  maxScale: 2}}>
-                                <TransformComponent wrapperClass={""}>
-                                    <TimelineGrid size={size}
-                                                  selectGridItemKey={selectGridItemKey}
-                                                  setSelectGridItemKey={setSelectGridItemKey}
-                                                  connectEntries={connectEntries}/>
-                                </TransformComponent>
-                            </TransformWrapper>
-                    </div>
+                    <DndProvider backend={HTML5Backend}>
+                        <div className={"timeline-content"}>
+                                <TransformWrapper disabled={disableDrag}
+                                                  minScale={0.5}
+                                                  maxScale={2}
+                                                  initialScale={1}
+                                                  limitToBounds={false}
+                                                  centerContent={false}>
+                                    <TransformComponent wrapperClass={""}>
+                                        <TimelineGrid size={size}
+                                                      selectGridItemKey={selectGridItemKey}
+                                                      setSelectGridItemKey={setSelectGridItemKey}
+                                                      connectEntries={connectEntries}/>
+                                    </TransformComponent>
+                                </TransformWrapper>
+                        </div>
+                    </DndProvider>
 
 
                     <div className="fill-container">
-                        <EntryCard size={size}/>
+                        <EntryCard/>
                         <SideBar/>
                     </div>
                 </EntryCardContext.Provider>
